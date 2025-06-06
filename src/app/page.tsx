96d473654501d1c2b2a27a5872000815dbf42185
Res0ution <maestro@hotger.com>
2025-06-06 17:20:01 +0300

    #2 create auth form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,53 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
 export default function Home() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email({
+      email,
+      name,
+      password
+    },
+    {
+      onError: () => {
+
+      },
+      onSuccess: () => {
+
+      }
+    }
+  )
+  }
+
   return (
-    <Button>
-      
-    </Button>
+    <div className="p-4 flex flex-col gap-y-4">
+      <Input
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button onClick={onSubmit}>
+        Create user
+      </Button>
+    </div>
   );
 }
