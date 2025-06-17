d752aedfb19af6374d424879dd4eba9e92e57fe6
Res0ution <maestro@hotger.com>
2025-06-17 22:52:20 +0300

    #5 dashboard module

--- src/app/modules/home/ui/views/home-view.tsx
+++ src/app/modules/home/ui/views/home-view.tsx
@@ -2,9 +2,13 @@
 
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 
 export const HomeView = () => {
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "Antonio"}));
   const router = useRouter();
   const { data: session } = authClient.useSession();
 
@@ -13,6 +17,7 @@ export const HomeView = () => {
   }
   return (
     <div className="flex flex-col p-4 gap-y-4">
+      {data?.greeting}
       <p>Logged in as {session.user.name}</p>
       <Button
         onClick={() =>
